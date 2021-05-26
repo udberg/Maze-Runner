0cf62426536aaab9b3722dae35d5c1f7fca4ed4c
udberg <johnuduak066@gmail.com>
2021-05-26 16:37:50 +0100

    Add pointer down function for the first scene on start

--- src/Objects/Button.js
+++ src/Objects/Button.js
@@ -10,4 +10,10 @@ export default class Button extends Phaser.GameObjects.Container {
     this.button = this.scene.add.sprite(0, 0, key1).setInteractive().setScale(1.3, 1);
     this.text = this.scene.add.text(0, 0, text, { fontSize: '32px', fill: '#fff' });
     Phaser.Display.Align.In.Center(this.text, this.button);
-    
+
+    this.add(this.button);
+    this.add(this.text);
+
+    this.button.on('pointerdown', () => {
+      this.scene.scene.start(targetScene);
+    });
