da7ffc7f3298af979817807c5dd4d48cd0d382ae
udberg <johnuduak066@gmail.com>
2021-05-26 16:36:26 +0100

    Set interactive scenes and add font size and fill colors

--- src/Objects/Button.js
+++ src/Objects/Button.js
@@ -6,4 +6,8 @@ export default class Button extends Phaser.GameObjects.Container {
     this.scene = scene;
     this.x = x;
     this.y = y;
+
+    this.button = this.scene.add.sprite(0, 0, key1).setInteractive().setScale(1.3, 1);
+    this.text = this.scene.add.text(0, 0, text, { fontSize: '32px', fill: '#fff' });
+    Phaser.Display.Align.In.Center(this.text, this.button);
     
