65b3930e27fcf316c91792bbf5f326f21971dbec
udberg <johnuduak066@gmail.com>
2021-05-26 12:02:27 +0100

    Calculate platform height

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,23 @@
 import Phaser from 'phaser';
 
 const fetch = require('node-fetch');
+
+export default {
+  calculateNextPlatformHeight: (previousPlatform, player, game) => {
+    let lowestPlatformHeight;
+    let highestPlatformHeight;
+
+    if (player) {
+      lowestPlatformHeight = Math.min(
+        game.config.height * 0.745,
+        player.y + 100,
+      );
+      if (player.y > 450) {
+        highestPlatformHeight = 425;
+      } else {
+        highestPlatformHeight = Math.max(
+          game.config.height * 0.275,
+          player.y - 75,
+        );
+      }
+    }
