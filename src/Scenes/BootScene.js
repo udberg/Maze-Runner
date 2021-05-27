268caca5478537504a6964194657f680f0943b6c
udberg <johnuduak066@gmail.com>
2021-05-27 13:11:44 +0100

    Preload scene with image

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -4,3 +4,10 @@ export default class BootScene extends Phaser.Scene {
   constructor() {
     super('Boot');
   }
+
+  preload() {
+    this.load.spritesheet('logo', 'assets/images/adventurer-sheet.png', {
+      frameWidth: 50,
+      frameHeight: 37,
+    });
+  }
