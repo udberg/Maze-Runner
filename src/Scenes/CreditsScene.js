0c7ccdb75c3ee53523d75c0aeb8e802cc03ea7a3
udberg <johnuduak066@gmail.com>
2021-05-27 13:49:04 +0100

    align contents

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -26,3 +26,29 @@ export default class CreditsScene extends Phaser.Scene {
       config.width,
       config.height,
     );
+
+    Phaser.Display.Align.In.Center(this.creditsText, this.zone);
+    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
+
+    this.madeByText.setY(1000);
+
+    this.creditsTween = this.tweens.add({
+      targets: this.creditsText,
+      y: -100,
+      ease: 'Power1',
+      duration: 3000,
+      delay: 1000,
+    });
+
+    this.madeByTween = this.tweens.add({
+      targets: this.madeByText,
+      y: -300,
+      ease: 'Power1',
+      duration: 8000,
+      delay: 1000,
+      onComplete: () => {
+        this.scene.start('Title');
+      },
+    });
+  }
+}
