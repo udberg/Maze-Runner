d80e51d256b197381c37450fa84ef49c765c4b3f
udberg <johnuduak066@gmail.com>
2021-05-27 13:46:28 +0100

    Fill author's name and other variables

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -5,3 +5,24 @@ export default class CreditsScene extends Phaser.Scene {
   constructor() {
     super('Credits');
   }
+
+  create() {
+    this.creditsText = this.add.text(0, 0, 'Credits', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.madeByText = this.add.text(
+      0,
+      0,
+      [
+        'Game Creation: Uduak John',
+        ' ',
+        'Music: Beedle The Bardcore'],
+      { fontSize: '26px', fill: '#fff' },
+    );
+    this.zone = this.add.zone(
+      config.width / 2,
+      config.height / 2,
+      config.width,
+      config.height,
+    );
