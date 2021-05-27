b975a779e302c6d1c587b20ea592c4577ecf81e8
udberg <johnuduak066@gmail.com>
2021-05-27 14:19:32 +0100

    Write test for different image lenths

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -25,3 +25,24 @@ describe("gets the path of a platform image depending on a platform's length", (
   const longPlatformLength = 201;
   const shortPlatformLength = 199;
   const randomPlatformLength = Phaser.Math.Between(100, 400);
+
+  it('always returns a valid image key', () => {
+    expect(
+      validKeys.includes(helpers.getPlatformImage(randomPlatformLength)),
+    ).toBe(true);
+  });
+
+  it('always gets the path of the longest platform image if the platform width is greater than 200px', () => {
+    expect(helpers.getPlatformImage(longPlatformLength) === 'plank3').toBe(
+      true,
+    );
+  });
+
+  it('always gets the path of one of the shorter platform images if the platform width is less than 200px', () => {
+    expect(
+      ['plank1', 'plank2'].includes(
+        helpers.getPlatformImage(shortPlatformLength),
+      ),
+    ).toBe(true);
+  });
+});
