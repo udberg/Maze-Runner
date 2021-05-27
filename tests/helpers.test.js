8a1b1d03d37a3f48c9af885a5531cb957a326d9e
udberg <johnuduak066@gmail.com>
2021-05-27 14:21:50 +0100

    Get leaderboard data from leaderboard API

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -62,3 +62,24 @@ describe("saves an input's value to localStorage", () => {
     expect(localStorage.getItem('username') === 'Anonymous').toBe(true);
   });
 });
+
+describe('fetches leaderboard data from the leaderboard API', () => {
+  it("returns a JSON object with a key of 'result'", () => helpers.fetchScores().then(jsonObject => {
+    expect(
+      Object.prototype.hasOwnProperty.call(jsonObject, 'result'),
+    ).toBeTruthy();
+  }));
+
+  it("returns a JSON object with an array object as the value of 'result'", () => helpers.fetchScores().then(jsonObject => {
+    expect(Array.isArray(jsonObject.result)).toBeTruthy();
+  }));
+
+  it("has objects stored within the 'result' array, each with a name and score", () => helpers.fetchScores().then(jsonObject => {
+    if (jsonObject.result.length === 0) return true;
+
+    return jsonObject.result.forEach(element => {
+      expect(Object.prototype.hasOwnProperty.call(element, 'user')).toBeTruthy();
+      expect(Object.prototype.hasOwnProperty.call(element, 'score')).toBeTruthy();
+    });
+  }));
+});
