c543d9a232c19a7288edd0b44033864edca68128
udberg <johnuduak066@gmail.com>
2021-05-28 12:12:21 +0100

    Preload graphics

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -9,3 +9,10 @@ export default class PreloaderScene extends Phaser.Scene {
   init() {
     this.readyCount = 0;
   }
+
+  preload() {
+    this.add.sprite(400, 175, 'logo', Phaser.Math.Between(0, 64)).setScale(3);
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
