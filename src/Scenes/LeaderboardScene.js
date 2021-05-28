f3054d117711c9b2d6f26472c77c21812d8a24ef
udberg <johnuduak066@gmail.com>
2021-05-28 12:34:14 +0100

    Fetch scores asynchronously

--- src/Scenes/LeaderboardScene.js
+++ src/Scenes/LeaderboardScene.js
@@ -5,3 +5,9 @@ export default class LeaderboardScene extends Phaser.Scene {
   constructor() {
     super('Leaderboard');
   }
+
+  async create() {
+    const scores = await helpers.fetchScores().then(
+      scoresObject => scoresObject.result,
+    );
+    const topScores = scores.sort((a, b) => b.score - a.score).slice(0, 10);
