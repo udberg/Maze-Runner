f8ec6a2a05843e1fe66877fb1300878d1e235d68
udberg <johnuduak066@gmail.com>
2021-05-28 13:20:18 +0100

    Create game parameters

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,3 +7,16 @@ export default class GameScene extends Phaser.Scene {
     super('Game');
     this.gameOptions = gameOptions;
   }
+
+  create() {
+    this.secondsElapsed = 0;
+    this.secondsRemaining = 60;
+    this.score = 0;
+    this.playerJumps = this.gameOptions.jumps;
+    this.coinGroup = this.add.group();
+    this.collectedCoinGroup = this.add.group();
+    this.physics.resume();
+    this.anims.resumeAll();
+    this.events.once('gameOver', () => {
+      this.gameOver();
+    });
