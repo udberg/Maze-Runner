a7154d5e9cbd78b4bd6ee2457ed719184adad3ce
udberg <johnuduak066@gmail.com>
2021-05-28 12:12:57 +0100

    Prelod text

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -16,3 +16,16 @@ export default class PreloaderScene extends Phaser.Scene {
     const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
