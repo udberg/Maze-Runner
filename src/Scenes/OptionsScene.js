ae6c5ff21d383749dc72c9eb83b6e0ce6a777d21
udberg <johnuduak066@gmail.com>
2021-05-28 12:41:51 +0100

    Create global game model and instructions

---
+++ src/Scenes/OptionsScene.js
@@ -0,0 +1,10 @@
+import Phaser from 'phaser';
+import Button from '../Objects/Button';
+
+export default class OptionsScene extends Phaser.Scene {
+  constructor() {
+    super('Options');
+  }
+
+  create() {
+    this.model = this.sys.game.globals.model;
