084c8cdd94a0765791639bab9fd87aa3da199bed
udberg <johnuduak066@gmail.com>
2021-05-28 12:11:27 +0100

    Initialize preloader scene

---
+++ src/Scenes/PreloaderScene.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+import helpers from '../helpers';
+
+export default class PreloaderScene extends Phaser.Scene {
+  constructor() {
+    super('Preloader');
+  }
+
+  init() {
+    this.readyCount = 0;
+  }
