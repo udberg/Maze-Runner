999782a95f481b6ee9414ac85604597cda69ca17
udberg <johnuduak066@gmail.com>
2021-05-28 12:42:55 +0100

    Automatically enable sound button

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -12,3 +12,6 @@ export default class OptionsScene extends Phaser.Scene {
     this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
     this.musicButton = this.add.image(200, 200, 'checkedBox');
     this.musicText = this.add.text(250, 190, 'Music Enabled', { fontSize: 24 });
+
+    this.soundButton = this.add.image(200, 300, 'checkedBox');
+    this.soundText = this.add.text(250, 290, 'Sound Enabled', { fontSize: 24 });
